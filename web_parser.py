687d9dbe7b333e8d68f829548ad2adf1686e2417
Daniel Hochmuth <sleepy.locust@gmail.com>
2021-02-03 08:33:27 +0100

    Working on parsing

--- web_parser.py
+++ web_parser.py
@@ -1,11 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://finance.yahoo.com/quote/BIIB/key-statistics?p=BIIB'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+base_url = 'https://finance.yahoo.com/quote/QUOTE/key-statistics?p=QUOTE'
+quotes = ['INTC', 'BIIB', 'MKL']
 
-results = soup.find(id="quote-header-info")
 
-job_elems = results.find_all('span', class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)')
-[print(elem.text.strip()) for elem in job_elems]
+for quote in quotes:
+
+    page = requests.get(base_url.replace('QUOTE', quote))
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Get price
+    header = soup.find(id="quote-header-info")
+    elems_price = header.find_all('span', class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)')
+    # print(type(elems_price))
+    [print(f'{quote}: {elem.text.strip()}') for elem in elems_price]
+
+    val_table = soup.find(id='mrt-node-Col1-0-KeyStatistics')
+    elems_pe_ratio = val_table.find_all('tr', class_='Bxz(bb) H(36px) BdB Bdbc($seperatorColor) fi-row Bgc($hoverBgColor):h')
+    # [print(elem) for elem in elems_pe_ratio]
+    print(val_table.find('td', attrs={'data-reactid':'28'}).text.strip())
