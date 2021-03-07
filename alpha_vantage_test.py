03df2a668b9d6edaf7377bfa74ca512d03c6fb4e
Daniel Hochmuth <sleepy.locust@gmail.com>
2021-03-07 15:01:09 +0100

    ...

--- alpha_vantage_test.py
+++ alpha_vantage_test.py
@@ -5,12 +5,13 @@ import secrets
 API_URL = 'https://www.alphavantage.co/query'
 symbol = 'GOOG'
 
-data = {'function': 'TIME_SERIES_DAILY',
+data = {'function': 'INCOME_STATEMENT',
         'symbol': symbol,
         'outputsize': 'full',
         'datatype': 'json',
         'apikey': secrets.alpha_vantage_key}
 
-response = requests.get(API_URL, data)
+response = requests.get(API_URL, params=data)
+assert response.ok, 'Error in getting response from server'
+print('Ok')
 
-print(response.json())
