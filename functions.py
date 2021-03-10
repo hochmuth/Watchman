fefb1dca96d2773f047e8b336d771792e52702e4
Daniel Hochmuth <sleepy.locust@gmail.com>
2021-03-10 21:45:45 +0100

    moved the API query to a script, improved the API call limit check

---
+++ functions.py
@@ -0,0 +1,75 @@
+# Functions used in the Watchman project
+import os
+import requests
+import datetime
+import time
+import pandas as pd
+import secrets
+
+from ratelimit import limits, sleep_and_retry
+
+
+def annual_data_into_df(ticker, metric_map, json_data, trim_key=False):
+    df_annual = pd.DataFrame()
+    for statement in metric_map:
+        for metric in metric_map[statement]:
+            df_tmp = get_annual_data(data=json_data,
+                                     ticker=ticker,
+                                     fin_statement=statement,
+                                     fin_metric=metric).sort_index().astype('float')
+            if df_annual.empty:
+                df_annual = df_tmp
+            else:
+                df_annual = df_annual.join(df_tmp, how='inner')
+    df_annual.sort_index(inplace=True)
+    if trim_key:
+        df_annual.index = df_annual.index.str.replace('-12-31', '')
+    return df_annual
+
+
+def api_data_function(symbol, function, apikey):
+    return {'function': function,
+            'symbol': symbol,
+            'outputsize': 'full',
+            'datatype': 'json',
+            'apikey': apikey}
+
+
+@sleep_and_retry
+@limits(calls=5, period=60)
+def call_api(url, data):
+    return requests.get(url, params=data)
+
+
+def df_to_csv(df, folder, ticker, report_type, year):
+    out_name = f'{folder}/{ticker}_{year}_{report_type}_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.csv'
+    df.to_csv(out_name, sep=';', encoding='utf8')
+
+
+def get_annual_data(data, ticker, fin_statement, fin_metric):
+    return pd.DataFrame.from_records([(record['fiscalDateEnding'], record[fin_metric])
+                                      for record in data[ticker][fin_statement]['annualReports']],
+                                     columns=['fiscalYear', fin_metric]).set_index('fiscalYear').replace('None', 0)
+
+
+def get_fundamentals_from_api(tickers, functions, api_url, api_key):
+    out_dict = dict()
+    for ticker in tickers:
+        fund_dict = dict()
+        for i, function in enumerate(functions):
+            # API limit
+            if i > 0 and i % 4 == 0:
+                time.sleep(60)
+            response = call_api(api_url, api_data_function(ticker, function, api_key))
+            assert response.ok, f'Error in getting response from server. Ticker {ticker}, function {function}'
+            fund_dict[function] = response.json()
+        out_dict[ticker] = fund_dict
+        # API limit
+        time.sleep(60)
+    return out_dict
+
+
+def overview_into_df(ticker, json_data):
+    df = pd.Series(dict(json_data[ticker]['OVERVIEW'])).to_frame().rename(columns={0: 'Value'})
+    df.index.rename('Metric', inplace=True)
+    return df
---
+++ gather_from_alpha_vantage.py
@@ -0,0 +1,56 @@
+'''Gathers fundamental information about a stock using the Alpha Vantage API.'''
+import os
+import requests
+import datetime
+import time
+import pandas as pd
+import secrets
+
+from ratelimit import limits, sleep_and_retry
+from functions import *
+from metrics import metric_map
+
+API_URL = 'https://www.alphavantage.co/query'
+api_key = secrets.ALPHAVANTAGE_API_KEY
+api_tickers = ['BIIB', 'GOOG', 'INTC']
+
+api_functions = ['OVERVIEW',
+                 'INCOME_STATEMENT',
+                 'BALANCE_SHEET',
+                 'CASH_FLOW']
+
+if __name__ == '__main__':
+
+    # API rate limit
+    if len(api_tickers) > 1:
+        time.sleep(60)
+    start_time = time.time()
+
+    # Query the API
+    api_results = get_fundamentals_from_api(tickers=api_tickers,
+                                            functions=api_functions,
+                                            api_url=API_URL,
+                                            api_key=api_key)
+    for ticker in api_tickers:
+        # Save the annual results into csv
+        df_annual = annual_data_into_df(ticker, metric_map, api_results, trim_key=True)
+        df_to_csv(df=df_annual,
+                  folder='./annual_reports',
+                  ticker=ticker,
+                  report_type='annual',
+                  year=df_annual.index.max())
+
+        # Save overview into csv
+        df_overview = overview_into_df(ticker, api_results)
+        df_to_csv(df=overview_into_df(ticker, api_results),
+                  folder='./overviews',
+                  ticker=ticker,
+                  report_type='overview',
+                  year=df_overview.loc['LatestQuarter'].values[0][:4])
+
+    # How long it took
+    running_time = time.time() - start_time
+    if running_time < 60:
+        print(f'Total running time: {round(running_time)} sec')
+    else:
+        print(f'Total running time: {round(running_time/60)} min')
